276f8783f42f489577e24de8f7d194a331cafa3a
Gabriel Sinay <gsinayc@gmail.com>
2022-07-09 14:45:22 -0400

    fixed static

--- Quasi/Quasi/settings.py
+++ Quasi/Quasi/settings.py
@@ -17,7 +17,7 @@ from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATIC_DIR = os.path.join(BASE_DIR, "Static")
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 MEDIA_DIR = os.path.join(BASE_DIR, "media")
 
