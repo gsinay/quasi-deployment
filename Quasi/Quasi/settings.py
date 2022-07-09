7434e7bfc8bb4100148f7f23148ab5cdfe224cfa
Gabriel Sinay <gsinayc@gmail.com>
2022-07-09 13:13:33 -0400

    tweaked for heroku

--- Quasi/Quasi/settings.py
+++ Quasi/Quasi/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_on_heroku 
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,9 +29,9 @@ MEDIA_DIR = os.path.join(BASE_DIR, "media")
 SECRET_KEY = '*rwpp&!3+%vtil_txz5-9bt5zp63oagvy-p$)aldi$7(y*rbpc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["quasinay.herokuapp.com", "127.0.0.1",]
 
 
 # Application definition
@@ -45,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,10 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = STATIC_DIR
 STATICFILES_DIRS = [
     STATIC_DIR, 
 ]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 #MEDIA
@@ -142,3 +147,5 @@ MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
 LOGIN_URL = '/base_app/login'
+
+django_on_heroku.settings(locals())
