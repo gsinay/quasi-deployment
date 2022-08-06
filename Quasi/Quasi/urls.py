bcab6cb7c7a09166bbf9f94aefa2a3bdd475ca70
Gabriel Sinay <gsinayc@gmail.com>
2022-08-06 12:55:48 -0400

    really fixed profile pics

--- Quasi/Quasi/urls.py
+++ Quasi/Quasi/urls.py
@@ -13,13 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from base_app import views
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name="index"),
     url(r'^base_app/', include("base_app.urls")),
     url(r'^logout$', views.user_logout, name = "logout")
-]
+] +static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
--- Quasi/base_app/models.py
+++ Quasi/base_app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class UserProfileinfo(models.Model):
     user = models.OneToOneField(User)
     date_of_birth = models.DateField()
-    profile_pic = models.ImageField(upload_to = 'profile_pics', blank = True)
+    profile_pic = models.ImageField(null = True, blank = True)
 
     def __str__(self):
         return self.user.username
